9aff090ad18127a2807b35454890d88a25c24ebc
DrewGodden <35704776+DrewGodden@users.noreply.github.com>
2019-03-31 00:05:49 -0400

    Implemented main menu functionality

---
+++ GetToKnowYa/Assets/Get-to-Know-Ya/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnStartPressed()
+    {
+        //Load the "Question" scene when the "start!" button is pressed
+        SceneManager.LoadScene("Question", LoadSceneMode.Single);
+    }
+
+    public void OnExitPressed()
+    {
+        //Quit the application when the exit button is pressed
+        Application.Quit();
+    }
+}
