a102e29b1f681ea9cbbc5c8077cc13db74331aa0
Pitchipsy <48604265+Pitchipsy@users.noreply.github.com>
2019-04-01 15:30:55 -0400

    Those sweet sweet wips
    lemmie know if i wrecked everything

---
+++ GetToKnowYa/DestroyQuestions.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyQuestions : MonoBehaviour
+{
+    public int questionHealth;
+    string[] ChoicesMade = new string[] { };
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (questionHealth <= 0)
+        {
+            OnQuestionDeath();
+        }
+    }
+
+    public float Damage()
+    {
+        return 0.0f;
+    }
+
+    public void OnQuestionDeath()
+    {
+   
+    }
+}
+
+
---
+++ GetToKnowYa/QuestionSystem.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class QuestionSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    string[] Choices1 = new string[] { "Hot Pepper", "Cats", "Dogs","Cake", "Ketchup" , "Sushi", "B-Movies"};
+    string[] Choices2 = new string[] { "Peanuts", "Drag Queens", "Ska Music", "Baby Boomers", "Black Jelly Beans", "Sake"};
+
+    public TMP_Text choice2Text;
+    public TMP_Text choice1Text;
+    float timeLeft = 5.0f;
+    bool choiceDestroyed = true;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        //displays after timer
+        //goes away and resets timer after destroyed
+        if (choiceDestroyed == true)
+        {
+
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0)
+            {
+                getChoices();
+                choiceDestroyed = false;
+
+                //needs if statment for detecting bullets 
+
+
+            }
+        }
+
+        Debug.Log(timeLeft);
+    }
+
+    public void getChoices() {
+  //      string[] Choices = new string[] { "Hot Pepper", "Cats", "Dogs", "Cake", "Ketchup" };
+
+        string choice1 = Choices1[Random.Range(0, Choices1.Length)];
+        string choice2 = Choices2[Random.Range(0, Choices2.Length)];
+        choice1Text.text = choice1;
+        choice2Text.text = choice2;
+        Debug.Log(choice1);
+
+
+    }
+
+}
