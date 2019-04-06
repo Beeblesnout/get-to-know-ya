f5df2ebffc927f0099ac7d655ead3a3f473b3951
Adam Gill <30602106+Beeblesnout@users.noreply.github.com>
2019-04-05 21:33:46 -0400

    owo

--- GetToKnowYa/Assets/Get-to-Know-Ya/Scripts/MultiplayerManager.cs
+++ GetToKnowYa/Assets/Get-to-Know-Ya/Scripts/MultiplayerManager.cs
@@ -4,6 +4,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MultiplayerManager : MonoBehaviour {
-    
+public class MultiplayerManager : SingletonBase<MultiplayerManager> 
+{
+    public static Action<Message> onMessage;
+
+    private void OnEnable()
+    {
+        Net.NetworkReceiveEvent += OnReceive;
+    }
+
+    private void OnDisable()
+    {
+        Net.NetworkReceiveEvent -= OnReceive;
+    }
+
+    private void OnReceive(NetConnection connection, Message message)
+    {
+        if (connection.connectId == Net.LocalConnectionID)
+        {
+            onMessage?.Invoke(message);
+        }
+        else
+        {
+            if (Net.IsServer)
+            {
+                message.Send();
+            }
+        }
+    }
+}
+
+public enum NetMessageType
+{
+    PlayerAliveState, PlayerPosition, PlayerEulerAngles, PlayerShootState, PlayerGunID, PlayerTeam
 }
