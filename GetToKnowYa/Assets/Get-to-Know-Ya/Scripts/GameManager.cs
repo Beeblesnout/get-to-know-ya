5107778d443f9b6cd34322227ff38fef30141a59
DrewGodden <35704776+DrewGodden@users.noreply.github.com>
2019-04-12 20:34:36 -0400

    Implemented return menu in Enemy Test scene

--- GetToKnowYa/Assets/Get-to-Know-Ya/Scripts/GameManager.cs
+++ GetToKnowYa/Assets/Get-to-Know-Ya/Scripts/GameManager.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject pauseMenu;
+    private bool gamePaused = false;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +17,10 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Escape) == true && SceneManager.GetActiveScene().name != "MainMenu")
+        {
+            EscapeMenu();
+        }
     }
 
     public void LoadNextScene()
@@ -37,5 +43,24 @@ public class GameManager : MonoBehaviour
     public void JoinGame()
     {
         Debug.Log("Join Game selected!");
+    }
+
+    public void EscapeMenu()
+    {
+        if(pauseMenu != null && gamePaused == false)
+        {
+            pauseMenu.SetActive(true);
+            gamePaused = true;
+        }
+        else if (pauseMenu != null && gamePaused == true)
+        {
+            pauseMenu.SetActive(false);
+            gamePaused = false;
+        }
+    }
+
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
     }
 }
